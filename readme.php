3668a9c7644b9ecc963fce240b8326063477a223
jigyxsa <98165883+jigyxsa@users.noreply.github.com>
2022-07-04 13:47:11 +0530

    WebTask

---
+++ readme.php
@@ -0,0 +1,6 @@
+config.php - configure database
+login.php - allows user to login
+register.php - allow user to register
+welcome.php - if user is able to login redirect him to welcome page 
+logout.php - aloows user to logout
+ 
